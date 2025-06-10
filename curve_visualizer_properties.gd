c7fbd101b6103544842eaebb8b283e846fb7ad59
Allen Pestaluky <allenpestaluky@gmail.com>
2025-06-10 13:43:15 -0400

    Got rid of crossover point animation

--- curve_visualizer_properties.gd
+++ curve_visualizer_properties.gd
@@ -9,11 +9,10 @@ extends Node
 @export var high_clip: float = 3.0
 @export var ref_luminance: float = 240
 @export var output_max_value: float = 1.0
-@export var crossover_point: float = 0.1841865
 @export var low_clip: float = 0.0
 @export var brightness: float = 0.0
 
-enum CURVE_FEATURE { contrast, high_clip, output_max_value, crossover_point, low_clip, brightness }
+enum CURVE_FEATURE { contrast, high_clip, output_max_value, low_clip, brightness }
 
 @export var curve_feature: CURVE_FEATURE = CURVE_FEATURE.contrast
 
@@ -25,8 +24,6 @@ func _ready() -> void:
 			animation.play("high_clip")
 		CURVE_FEATURE.output_max_value:
 			animation.play("output_max_value")
-		CURVE_FEATURE.crossover_point:
-			animation.play("crossover_point")
 		CURVE_FEATURE.low_clip:
 			animation.play("low_clip")
 		CURVE_FEATURE.brightness:
@@ -40,8 +37,6 @@ func _process(_delta: float) -> void:
 	curve_comparison.white = high_clip;
 	mesh.material_override.set("shader_parameter/output_max_value", output_max_value)
 	curve_comparison.max_value = output_max_value;
-	mesh.material_override.set("shader_parameter/awp_crossover_point", crossover_point)
-	curve_comparison.agxRefMiddleGrey = crossover_point;
 	
 	if curve_feature == CURVE_FEATURE.contrast:
 		label.text = "Contrast: %0.2f" % contrast
@@ -49,12 +44,6 @@ func _process(_delta: float) -> void:
 		label.text = "High Clip: %0.2f" % high_clip
 	if curve_feature == CURVE_FEATURE.output_max_value:
 		label.text = "Reference White\nLuminance: %.0f nits\n\nOutput Max Value: %0.2f\n(Max Luminance: %.0f nits)" % [ref_luminance, output_max_value, ref_luminance * output_max_value]
-	if curve_feature == CURVE_FEATURE.crossover_point:
-		%SideBySide_constant.text = "Constant Dynamic Range (SDR)\nCrossover Point: %0.2f\nOutput Max Value: 1.00" % crossover_point
-		%SideBySide_variable.text = "Variable Dynamic Range (EDR & HDR)\nCrossover Point: %0.2f\nOutput Max Value: %0.2f" % [crossover_point, output_max_value]
-		label.text = "Crossover Point: %0.2f" % crossover_point
-	
-	
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
