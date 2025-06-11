7a9804157d45e7aea654cf2d7eb74ee3fbbbfba2
Allen Pestaluky <allenpestaluky@gmail.com>
2025-06-11 15:52:50 -0400

    Created HDR output max demo animation

--- curve_visualizer_properties.gd
+++ curve_visualizer_properties.gd
@@ -2,13 +2,16 @@ extends Node
 
 @export var curve_comparison: Node
 @export var label: Label
+@export var out_max_label: Label
+@export var out_max_label_backing: CanvasItem
 @export var animation: AnimationPlayer
 @export var mesh: MeshInstance3D
 
 @export var contrast: float = 1.5
-@export var high_clip: float = 3.0
-@export var ref_luminance: float = 240
-@export var output_max_value: float = 1.0
+@export var high_clip: float = 12.0
+@export var ref_luminance: float = 100
+@export var max_luminance: float = 100
+var output_max_value: float = 1.0
 @export var low_clip: float = 0.0
 @export var brightness: float = 0.0
 
@@ -17,6 +20,8 @@ enum CURVE_FEATURE { contrast, high_clip, output_max_value, low_clip, brightness
 @export var curve_feature: CURVE_FEATURE = CURVE_FEATURE.contrast
 
 func _ready() -> void:
+	get_window().hdr_output_auto_adjust_reference_luminance = false
+	
 	match curve_feature:
 		CURVE_FEATURE.contrast:
 			animation.play("contrast")
@@ -30,6 +35,9 @@ func _ready() -> void:
 			animation.play("brightness")
 
 func _process(_delta: float) -> void:
+	output_max_value = max_luminance / ref_luminance
+	out_max_label.text = ""
+	out_max_label_backing.visible = false
 	
 	mesh.material_override.set("shader_parameter/awp_contrast", contrast)
 	curve_comparison.A = contrast;
@@ -38,12 +46,16 @@ func _process(_delta: float) -> void:
 	mesh.material_override.set("shader_parameter/output_max_value", output_max_value)
 	curve_comparison.max_value = output_max_value;
 	
+	get_window().hdr_output_reference_luminance = ref_luminance;
+	
 	if curve_feature == CURVE_FEATURE.contrast:
 		label.text = "Contrast: %0.2f" % contrast
 	if curve_feature == CURVE_FEATURE.high_clip:
 		label.text = "High Clip: %0.2f" % high_clip
 	if curve_feature == CURVE_FEATURE.output_max_value:
-		label.text = "Reference White\nLuminance: %.0f nits\n\nOutput Max Value: %0.2f\n(Max Luminance: %.0f nits)" % [ref_luminance, output_max_value, ref_luminance * output_max_value]
+		label.text = ""
+		out_max_label_backing.visible = true
+		out_max_label.text = "Reference White Luminance: %.0f nits\nMax Luminance: %.0f nits\nOutput Max Value: %0.2f" % [ref_luminance, max_luminance, output_max_value]
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
